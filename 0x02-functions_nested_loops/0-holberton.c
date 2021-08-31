79b4d2ad2cfd9be64bd2f6ff91b83bd675ba4601
menomi <melattekle@protonmail.com>
2021-08-31 15:09:11 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ *main- prints Holberton, followed by a new line
+ *Return:always 0 (Success)
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+file 0: a program that prints Holberton, followed by a new line.
+file 1:a function that prints the alphabet, in lowercase, followed by a new line.
+file 2:a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+file 3:a function that checks for lowercase character.
