f22b06a7575ab877be9b80cff2a1b493ab4ef1b9
menomi <melattekle@protonmail.com>
2021-08-31 15:35:31 +0300

    fix looping error

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,16 +6,14 @@
  */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-
+int i = 0;
+char msg[] = "Holberton\n";
+char c;
+while (i < 9)
+{
+c = msg[i];
+_putchar(c);
+i++;
+}
 return (0);
 }
