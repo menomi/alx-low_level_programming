db50fc7957322e919687bf3d0aaffaac6a876933
menomi <melattekle@protonmail.com>
2021-09-23 23:31:31 +0300

    fix multiply code, fix string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,32 +11,40 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int j, k, l;
 	char *str;
 
+	unsigned int i, j, len = n;
+
 	if (s1 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s1[j]; j++)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-		k = 0;
-	else
-	{
-		for (k = 0; s2[k]; k++)
-			;
-	}
-	if (k > n)
-		k = n;
-	str = malloc(sizeof(char) * (j + k + 1));
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		;
+
+	for (j = 0; s2[j]; j++)
+		;
+
+	if (j > n)
+		j = n;
+
+	str = malloc(sizeof(char) * (i + j + 1));
+
 	if (str == NULL)
 		return (NULL);
-	for (l = 0; l < j; l++)
-		str[l] = s1[l];
-	for (l = 0; l < k; l++)
-		str[l + j] = s2[l];
-	str[j + k] = '\0';
+
+	for (len = 0; len < i; len++)
+	{
+		str[len] = s1[len];
+	}
+
+	for (len = 0; len < j; len++)
+	{
+		str[len + i] = s2[len];
+	}
+
+	str[i + j] = '\0';
+
 	return (str);
 }
